4250eaad29bf990fadf967c6549ada6f5456c4cc
Esteban <esteban@attitude.cl>
2022-03-30 13:28:58 -0300

    updates

---
+++ plugins/safe-svg/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.9.10',
+        'version' => '1.9.10.0',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '2e3c5bd22d90169006413cf74ec3f4cc5baeaa7c',
+        'name' => 'darylldoyle/safe-svg',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'darylldoyle/safe-svg' => array(
+            'pretty_version' => '1.9.10',
+            'version' => '1.9.10.0',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '2e3c5bd22d90169006413cf74ec3f4cc5baeaa7c',
+            'dev_requirement' => false,
+        ),
+        'enshrined/svg-sanitize' => array(
+            'pretty_version' => '0.15.4',
+            'version' => '0.15.4.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../enshrined/svg-sanitize',
+            'aliases' => array(),
+            'reference' => 'e50b83a2f1f296ca61394fe88fbfe3e896a84cf4',
+            'dev_requirement' => false,
+        ),
+    ),
+);
