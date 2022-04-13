674755acf05b7eb54e98a8c14bb1a8bc6e226be8
Esteban <esteban@attitude.cl>
2022-04-13 15:21:20 -0400

    css/js cache bust

--- themes/hello-theme-child/functions.php
+++ themes/hello-theme-child/functions.php
@@ -11,13 +11,26 @@
  * @return void
  */
 function hello_elementor_child_enqueue_scripts() {
+	// Cache bust
+	$stylecss  = date('Ymd-Gis', filemtime( get_stylesheet_directory() . '/scripts.js' ));
+	$scriptsjs = date('Ymd-Gis', filemtime( get_stylesheet_directory() . '/style.css' ));
+
 	wp_enqueue_style(
 		'hello-elementor-child-style',
 		get_stylesheet_directory_uri() . '/style.css',
 		[
 			'hello-elementor-theme-style',
 		],
-		'1.0.0'
+		$stylecss
+	);
+
+	wp_enqueue_script(
+		'hello-elementor-child-script',
+		get_stylesheet_directory_uri() . '/scripts.js',
+		[
+			'jquery'
+		],
+		$scriptsjs
 	);
 }
 add_action( 'wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts', 20 );
